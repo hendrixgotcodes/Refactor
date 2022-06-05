6dd4ebec906dc7eb5da4754f205081e3dd8f8383
Samuel O. Asare <asare11samuel@gmail.com>
2022-06-05 16:34:03 +0000

    refactor: restructured codebase

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // #!/usr/bin/env node
+
 import yargs from "yargs";
 import renameCommand from "./commands/rename";
 import aboutCommand from "./commands/about";
+
 yargs
-    .command(renameCommand)
-    .command(aboutCommand)
-    .conflicts("files", "directory")
-    .version("0.0.1")
-    .parse();
+  .command(renameCommand)
+  .command(aboutCommand)
+  .conflicts("files", "directory")
+  .version("0.0.1")
+  .parse();

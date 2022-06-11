33ecdd6d19f7ba6735fe27cc80f25b7a7ccf0129
Samuel O. Asare <asare11samuel@gmail.com>
2022-06-11 02:24:40 +0000

    chore: test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,5 @@
-module.exports = {
-  /*
-   * Resolve and load @commitlint/config-conventional from node_modules.
-   * Referenced packages must be installed
-   */
+export default {
   extends: ["@commitlint/config-conventional"],
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
   rules: {
     "type-enum": [
       2,
